d063c91824782655f5bc63fc2852814d3a909f4f
Chip <fillpant@gmx.co.uk>
2023-01-27 13:40:19 +0000

    Fixed incorrect thread calculation for when the register-based kernel is used.

--- deffinitions.cuh
+++ deffinitions.cuh
@@ -24,7 +24,7 @@
 #define USE_REGISTER_ONLY_KERNEL
 
 //The number N for the puzzle.
-#define N 25
+#define N 18
 
 // Number of kernel launches for a given input to run. This is to be used when timing the kernels only. 
 // Since results are not cleared or accumulated between runs, this may result in incorrect (inflated) results.
@@ -32,6 +32,12 @@
 
 //The first N nsb's set. 
 #define N_MASK ((1U<<N)-1U)
+
+// If defined, memory allocations for generation and state loading would be done on perstitent storage. 
+// THIS OPTION IS ONLY SUPPORTED ON POSIX SYSTEMS WITH MMAP SUPPORT!
+// Persistent storage can cause more errors which are not currently handled propperly! Ensure enough storage is available and expect
+// 'experimental' behaviour
+//#define ENABLE_PERSISTENT_BACKED_MEMORY
 
 // If set to 1, state generation will stop increasing the locked row as soon as doing so results in fewer states being generated.
 // If set to 0, state generation will continue exploring until it gets as close to the target limit, or until the board is explored almost completely. 
@@ -61,12 +67,17 @@
 //If defined, states won't be aligned to any specific value (in the interest of memory saving, may help)
 #define MEM_SAVING_STATES
 
+#ifndef USE_REGISTER_ONLY_KERNEL
 //When using the single 'do it all' kernel, blocks are 1-dimensional. The size of the block impacts shared memory allocation. THIS VALUE MUST BE DIVISIBLE BY 32!
 //Shared memory requirements are this many nq_state_t's plus 32 unsigned ints (aligned) per block. The size of nq_state_t varies depending on N.
 //A stable definition was the bellow which acomodates all N up to 31, but produces smaller-than-possible blocks for smaller N. This in itself is no problem, and even desirable in some devices
 //#define COMPLETE_KERNEL_BLOCK_THREAD_COUNT 736
 //In other cases, the bellow dynamically computes the block size based on relative shared memory requirements (hopefully, the compiler will notice this is a constant expression and will replace by result)
 #define COMPLETE_KERNEL_BLOCK_THREAD_COUNT MIN(((SMEM_SIZE - (WARPS_PER_BLOCK * sizeof(unsigned))) / sizeof(nq_state_t)) - (((SMEM_SIZE - (WARPS_PER_BLOCK * sizeof(unsigned))) / sizeof(nq_state_t)) % WARP_SIZE), 1024)
+#else
+//(memsize/(per_thread_mem*32 + per_warp_mem))*32
+#define COMPLETE_KERNEL_BLOCK_THREAD_COUNT MIN((SMEM_SIZE/((WARP_SIZE*N)+(sizeof(unsigned)*WARP_SIZE)))*WARP_SIZE, 1024)
+#endif
 
 //Number of nq_state_t's in the initial buffer used for state generation. Pool will initially contain this many elements
 // and will be resized as needed in a linear fashion, in increments of this many elements (i.e. after the first resize it will be X+X many 
@@ -118,6 +129,10 @@
 								 } \
 								}
 
+#endif
+
+#if defined(ENABLE_PERSISTENT_BACKED_MEMORY) && (!defined(__unix__) || !defined(__APPLE__))
+#error "Persistent storage backed memory allocations are not supported in the current system!";
 #endif
 
 
